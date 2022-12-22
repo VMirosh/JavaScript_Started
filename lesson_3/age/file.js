5807e332200ba617c2c3c3028a2fbd29b1c1c488
Vladyslav Miroshnychenko <miroshnychenko.vlad@gmail.com>
2022-12-22 14:13:03 +0100

    addFileLesson_3

---
+++ lesson_3/age/file.js
@@ -0,0 +1,8 @@
+let ageUser = Number(prompt("How old are you- ", 0));
+if (ageUser >= 18) {
+  alert("Welcome");
+}
+else {
+  alert("The entrance is closed");
+}
+
---
+++ lesson_3/ternary/file.js
@@ -0,0 +1,10 @@
+let x = 10;
+let y = 0;
+
+y = x > 5 ? y = 20 : y = 30;
+
+// if (x > 5)
+//     y = 20;
+// else
+//     y = 30;
+console.log(y);
