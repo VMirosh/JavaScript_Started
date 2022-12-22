747cee268d8079ef38774a4b971f2c31456c7476
Vladyslav Miroshnychenko <miroshnychenko.vlad@gmail.com>
2022-12-22 14:11:42 +0100

    addFileLesson_2

---
+++ lesson_2/file-for-index2.js
@@ -0,0 +1,15 @@
+let a = 10;
+let b = 20;
+let c = "hello";
+
+ console.log(a == b);
+ console.log(a < b);
+ console.log(a > b);
+ console.log(a === b);
+ console.log(a !== b);
+ console.log(b === c);
+ console.log(b <= a);
+ console.log(b >= c);
+ console.log(a != 2);
+ console.log(b !== c);
+ 
